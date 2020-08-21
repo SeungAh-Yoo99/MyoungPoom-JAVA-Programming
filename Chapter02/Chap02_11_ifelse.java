5cf32d874528f0214d509512cef3d4a0a0b9f72a
DESKTOP-3KDESHE\승아 <ysa8497@gmail.com>
2020-08-22 00:38:04 +0900

    MyoungPoom JAVA Programming 실습문제
    Chapter02 11-(1)번

---
+++ Chapter02/Chap02_11_ifelse.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Chap02_11_ifelse {
+
+		public static void main(String args[]) {
+			
+			Scanner scan=new Scanner(System.in);
+			
+			System.out.print("달을 입력하세요(1~12)>>");
+			int month=scan.nextInt();
+			
+			if ((month>=1)&&(month<=12)) {
+				if((month==3)||(month==4)||(month==5))
+					System.out.println("봄");
+				else if((month==6)||(month==7)||(month==8))
+					System.out.println("여름");
+				else if((month==9)||(month==10)||(month==11))
+					System.out.println("가을");
+				else
+					System.out.println("겨울");
+				}
+			else
+				System.out.println("잘못입력");
+		}
+				
+}
