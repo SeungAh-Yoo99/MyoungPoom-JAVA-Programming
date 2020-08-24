aae2edaa834232aa36a35736c04a98df4da9c75c
DESKTOP-3KDESHE\승아 <ysa8497@gmail.com>
2020-08-24 22:48:21 +0900

    MyoungPoom JAVA Programming 실습문제
    Chapter03 1-(3)번

---
+++ Chapter03/Chap03_01_ForTest.java
@@ -0,0 +1,12 @@
+public class Chap03_01_ForTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int sum=0;
+		for(int i=0;i<100;i+=2)
+			sum+=i;
+		System.out.println(sum);
+	}
+
+}
